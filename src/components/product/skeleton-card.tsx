a0b51fd5629363652c6ecce6a587ff9706bc64b7
Budi Gunawan <budiindra.g@gmail.com>
2024-07-16 18:45:45 +0700

    fix: adjust skeleton card styling

--- src/components/product/skeleton-card.tsx
+++ src/components/product/skeleton-card.tsx
@@ -2,11 +2,13 @@ import { Skeleton } from "../ui/skeleton";
 
 export const SkeletonCard = () => {
   return (
-    <div className="flex flex-col space-y-3 lg:col-span-3 md:col-span-4 col-span-6">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+    <div className="lg:col-span-3 md:col-span-4 col-span-6">
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[188px]" />
+        <div className="space-y-2">
+          <Skeleton className="h-4" />
+          <Skeleton className="h-4" />
+        </div>
       </div>
     </div>
   );
