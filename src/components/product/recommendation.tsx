f01d193aeb834b9d5c359bb79b592c2c85e2cee3
Budi Gunawan <budiindra.g@gmail.com>
2024-07-17 09:02:30 +0700

    feat: add recommendation component

---
+++ src/components/product/recommendation.tsx
@@ -0,0 +1,60 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { ProductCard } from "../product/product-card";
+import { Product } from "@/lib/types";
+import { SkeletonCard } from "../product/skeleton-card";
+
+type RecommendationParams = {
+  products: Product[];
+  isLoading: boolean;
+  page: string;
+};
+
+export const Recommendation = ({
+  products,
+  isLoading,
+  page,
+}: RecommendationParams) => {
+  const navigate = useNavigate();
+
+  const hanldeViewMore = () => {
+    navigate("/products");
+  };
+
+  return (
+    <section className="p-4 md:p-0">
+      <h2 className="font-bold font-poppins text-4xl text-b-black text-center uppercase">
+        {page === "details" ? "People also viewed" : "People also bought"}
+      </h2>
+      {isLoading ? (
+        <div className="my-10">
+          <div className="grid gap-x-4 gap-y-8 grid-cols-12">
+            {[...Array(8)].map((_e, i) => (
+              <SkeletonCard key={i} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="my-10">
+          {products?.length === 0 && <h3>Collection is empty</h3>}
+          {products?.length > 0 && (
+            <>
+              <div className="grid gap-x-4 gap-y-8 grid-cols-12">
+                {products?.map((product, index) => (
+                  <ProductCard key={index} product={product} />
+                ))}
+              </div>
+              <Button
+                className="w-full my-6 border-black rounded-none uppercase font-plus"
+                variant="outline"
+                onClick={hanldeViewMore}
+              >
+                View more
+              </Button>
+            </>
+          )}
+        </div>
+      )}
+    </section>
+  );
+};
--- src/pages/product-detail.tsx
+++ src/pages/product-detail.tsx
@@ -1,10 +1,42 @@
+import { getAllProduct } from "@/api/products-api";
 import { Layout } from "@/components/layout/layout";
 import { Detail } from "@/components/product/detail";
+import { Recommendation } from "@/components/product/recommendation";
+import { Product } from "@/lib/types";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const ProductDetail = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getAllProduct({ page: 1, limit: 5 });
+
+      if (response?.status === "success") {
+        const responseProducts = response?.data?.products;
+        const filteredProducts = responseProducts
+          .filter((product: Product) => product.id !== id)
+          .slice(0, 4);
+        setProducts(filteredProducts);
+      }
+
+      setIsLoading(false);
+    }
+
+    fetchData();
+  }, [id]);
+
   return (
     <Layout>
       <Detail />
+      <Recommendation
+        products={products}
+        isLoading={isLoading}
+        page="details"
+      />
     </Layout>
   );
 };
