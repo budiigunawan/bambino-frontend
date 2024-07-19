9f8adb4b4a72e235dac7e1f8269c13ab64dca799
Budi Gunawan <budiindra.g@gmail.com>
2024-07-19 15:30:00 +0700

    fix: rename store and add loading on app file

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "./navbar";
 import { Footer } from "./footer";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import { useEffect } from "react";
-import { usePageStore } from "@/stores/store";
+import { usePageStore } from "@/stores/page";
 
 export const Layout = () => {
   const { pageLoaded } = usePageStore();
@@ -17,7 +17,7 @@ export const Layout = () => {
       <Navbar />
       <div className="xl:max-w-6xl lg:max-w-4xl md:max-w-2xl mx-auto">
         <div className="lg:min-h-[824px] min-h-[700px]">
-          {pageLoaded ? <Outlet /> : <p>Loading...</p>}
+          {pageLoaded ? <Outlet /> : <p>Loading....</p>}
         </div>
       </div>
       <Footer />
