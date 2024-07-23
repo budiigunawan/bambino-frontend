dd8e7e54a613b381ded979e4e5a94fd14e75248f
Budi Gunawan <budiindra.g@gmail.com>
2024-07-23 16:50:04 +0700

    feat: adjust navbar for profile

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,8 +1,19 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 import { Link } from "react-router-dom";
 import { CiUser } from "react-icons/ci";
 import { IoCartOutline } from "react-icons/io5";
+import { useUserStore } from "@/stores";
 
 export const Navbar = () => {
+  const user = useUserStore((state) => state.user);
+  const cartId = localStorage.getItem("cartId");
+
   return (
     <div className="border-b border-b-gray-light fixed top-0 w-full z-10 bg-white">
       <div className="xl:max-w-6xl lg:max-w-4xl md:max-w-2xl mx-auto h-14 flex justify-between md:p-0 px-4">
@@ -12,18 +23,36 @@ export const Navbar = () => {
             Bambino
           </p>
         </Link>
-        <ul className="py-4 flex justify-end gap-6 md:text-2xl text-xl">
-          <li>
-            <Link to={"/profile"}>
-              <CiUser />
-            </Link>
-          </li>
-          <li>
-            <Link to={"/cart"}>
-              <IoCartOutline />
-            </Link>
-          </li>
-        </ul>
+        <div className="py-4">
+          <Menubar className="p-0 border-0 h-6 gap-4">
+            <MenubarMenu>
+              {user ? (
+                <>
+                  <MenubarTrigger className="p-1">
+                    <CiUser fontSize={"24px"} />
+                  </MenubarTrigger>
+                  <MenubarContent>
+                    <MenubarItem>
+                      <Link to="/profile" className="w-full">
+                        Profile
+                      </Link>
+                    </MenubarItem>
+                    <MenubarItem>Logout</MenubarItem>
+                  </MenubarContent>
+                </>
+              ) : (
+                <Link to="/login" className="p-1">
+                  <CiUser fontSize={"24px"} />
+                </Link>
+              )}
+            </MenubarMenu>
+            <MenubarMenu>
+              <Link to={cartId ? "/cart" : "/login"}>
+                <IoCartOutline fontSize={"24px"} />
+              </Link>
+            </MenubarMenu>
+          </Menubar>
+        </div>
       </div>
     </div>
   );
